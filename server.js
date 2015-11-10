cdde1d08667ca2edd7c36df4f5ea8e1f522049cc
Chris Hansen <Chansen88@me.com>
2015-11-10 10:01:42 -0800

    Added file structure

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('cookie-session');
+var db = require('./models');
