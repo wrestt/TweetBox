9585d5183241b923a7895bb354ebdf5f13033f21
Chris Hansen <Chansen88@me.com>
2015-11-10 10:08:51 -0800

    Add more to file structure

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('cookie-session');
 var db = require('./models');
+
+app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+app.use(session({
+  maxAge: 36000000,
+  secret: 'hurhurhurhur',
+  name: 'TweetBox'
+}));
+
+require('./controllers/index');
+
+app.listen(3000, function() {
+  console.log('Server up @ ' +  3000);
+});
