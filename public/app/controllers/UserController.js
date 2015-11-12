fce74d1f734ef6ef516452825307b1b36f36bf44
Teagan Wrest <wrestteagan@gmail.com>
2015-11-12 11:08:01 -0800

    Added Users Controller

--- public/app/controllers/UserController.js
+++ public/app/controllers/UserController.js
@@ -1,4 +1,14 @@
 (function() {
   angular
     .module('tweetBoxApp')
-    .controller('LoginController', ['$scope', 'Spotify', '$http',
+    .controller('UsersController', ['$scope', 'UserData',
+      function($scope, UserData) {
+        $scope.user = UserData.UserData;
+
+        $scope.addUser = function(obj) {
+          UserData.addUser($scope.newUser);
+          $scope.newUser = {};
+        };
+      }
+    ]);
+});
