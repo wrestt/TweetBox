edb74e8c5883c2e45d7a1d967f14bf4d8d8a06eb
Teagan Wrest <wrestteagan@gmail.com>
2015-11-11 17:10:19 -0800

    Working with anuglar login page

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -1,30 +1,27 @@
 (function() {
   angular
     .module('tweetBoxApp', [])
-    .config(function (SpotifyProvider) {
+    .config(function(SpotifyProvider) {
       SpotifyProvider.setClientId('123456789123456789');
       SpotifyProvider.setRedirectUri('http://example.com/callback.html');
       SpotifyProvider.setScope('playlist-read-private');
     })
-    .controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
-
-      $scope.searchArtist = function() {
-        Spotify.search($scope.searchartist, 'artist').then(function (data) {
-          $scope.artists = data.artists.items;
-        });
-      };
-      $scope.login = function() {
-        Spotify.login().then(function(data) {
+    .controller('MainController', ['$scope', 'Spotify',
+      function($scope, Spotify) {
+        $scope.searchArtist = function() {
+          Spotify.search($scope.searchartist, 'artist').then(function(data) {
+            $scope.artists = data.artists.items;
+          });
+        };
+        $scope.login = function() {
+          Spotify.login().then(function(data) {
+            console.log(data);
+            alert('You are now logged in');
+          });
+        };
+        Spotify.getTrack('0eGsygTp906u18L0Oimnem').then(function(data) {
+          console.log('=================== Track ===================');
           console.log(data);
-          alert("You are now logged in");
-        })
-      };
-      
-        Spotify.getTrack('0eGsygTp906u18L0Oimnem').then(function (data) {
-        console.log('=================== Track ===================');
-        console.log(data);
-      });
-
-    }]);
-
+        });
+      }]);
 })();
