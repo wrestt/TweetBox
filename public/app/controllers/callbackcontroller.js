e21c7cf3a6febab4bfc0708b85c7d6e803fcfc9b
Teagan Wrest <wrestteagan@gmail.com>
2015-11-16 11:59:02 -0800

    added iframe playlist binding

--- public/app/controllers/callbackcontroller.js
+++ public/app/controllers/callbackcontroller.js
@@ -1,10 +1,13 @@
 (function() {
   angular
     .module('tweetBoxApp')
-    .controller('CallBackController', ['$scope', '$window',
-      function($scope, $window) {
+    .controller('CallBackController', ['$scope', '$window', '$cookies',
+      function($scope, $window, $cookies) {
         $scope.close = function() {
-          $window.close();
+          // $window.close()
+          console.log('****************************ksjdgfjksdkjhgfkjgsdfjkgsdfkjgh');
+          var value = $cookies.get();
+          console.log(value);
         };
       }
     ]);
