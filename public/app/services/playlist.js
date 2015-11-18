562173926749b1342cebf28c62bbd1bea7f4b304
Teagan Wrest <wrestteagan@gmail.com>
2015-11-17 23:47:46 -0800

    modified search results and added track remove button and function

--- public/app/services/playlist.js
+++ public/app/services/playlist.js
@@ -55,16 +55,28 @@
               );
             }
             Playlist.sort();
-            Playlist.buildUrl();
           }
         });
       };
 
       Playlist.addSearch = function(songdata) {
-        var newTrack = new Track(songdata, false);
-        newTrack.score = 0;
-        newTrack.time = Date.now();
-        Playlist.trackData.push(newTrack);
+        console.log(songdata);
+        if (songdata[0]) {
+          var temp = _.find(
+            Playlist.trackData, _.matchesProperty(
+              'id', songdata[0].id
+            )
+          );
+          if (temp) {
+            temp.score ++;
+          } else {
+            var newTrack = new Track(songdata, false);
+            newTrack.score = 0;
+            newTrack.time = Date.now();
+            Playlist.trackData.push(newTrack);
+          }
+          Playlist.sort();
+        };
       };
 
       Playlist.buildUrl = function() {
@@ -106,17 +118,22 @@
             return 1;
           }
           if (a.time > b.time) {
-            return -1;
-          } else {
             return 1;
+          } else {
+            return -1;
           }
         }
+        Playlist.buildUrl();
       };
 
       Playlist.scoreChange = function(track, adj) {
         track.score += adj;
         Playlist.sort();
-        Playlist.buildUrl();
+      };
+
+      Playlist.remove = function(track) {
+        _.difference(Playlist.trackData, track);
+        Playlist.sort();
       };
 
       return Playlist;
