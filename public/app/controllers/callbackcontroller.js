735645d115520f8cd44344604ea94247e38ef021
Teagan Wrest <wrestteagan@gmail.com>
2016-01-13 12:28:30 -0800

    fixing bug

--- public/app/controllers/callbackcontroller.js
+++ public/app/controllers/callbackcontroller.js
@@ -3,8 +3,7 @@
     .module('tweetBoxApp')
     .controller('CallBackController', ['$scope', '$window',
       function($scope, $window) {
-        var vm = this;
-        vm.close = function() {
+        $scope.close = function() {
           $window.close();
         };
       }
